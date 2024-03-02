c293ebf8b1d25f2128e942ca31991df70d0b30a6
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2024-03-02 08:59:26 +0530

    Update GithubCodeCopy.java

--- Practice/GithubCodeCopy.java
+++ Practice/GithubCodeCopy.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GithubTest 
+public class GithubCodeCopy 
 {
 	public static void main(String[] args) 
 	{
