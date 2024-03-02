c64e543c884dbd4fd301da7213f45930353aca5c
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2024-03-02 08:24:35 +0530

    Create GithubLogin.java

---
+++ Practice/GithubLogin.java
@@ -0,0 +1,30 @@
+package Practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GithubLogin
+{
+	public static void main(String[] args) 
+	{
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://github.com/login");
+		
+		System.out.println(driver.getTitle());
+		
+		WebElement username = driver.findElement(By.id("login_field"));
+		WebElement password = driver.findElement(By.id("password"));
+		WebElement submitbutton = driver.findElement(By.name("commit"));
+		
+		username.sendKeys("tayyab7404"); // Use your username
+		password.sendKeys("tayyab@7404"); // Use your password
+		submitbutton.click();
+		
+		System.out.println(driver.getTitle());
+		
+		// driver.quit()
+	}
+}
