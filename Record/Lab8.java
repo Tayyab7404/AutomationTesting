638482f408d4af3a236ba5418c5425e2a88c6122
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2024-04-17 14:33:20 +0530

    Add files via upload

--- Record/Lab8.java
+++ Record/Lab8.java
@@ -2,10 +2,10 @@ package Record;
 
 import java.io.File;
 import java.io.IOException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.OutputType;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Lab8
---
+++ Record/Lab9a.java
@@ -0,0 +1,38 @@
+package Record;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Lab9a
+{
+	WebDriver driver = new ChromeDriver();
+	
+	@Test
+	public void testAssert()
+	{
+		driver.get("https://www.w3schools.com/");
+		
+		String ActualTitle = driver.getTitle();
+		String ExpectedTitle = "Most Reliable App & Cross Browser Testing Platform | browserstack";
+		
+		Assert.assertNotEquals(ActualTitle, ExpectedTitle);
+		System.out.println("The Actual Title is: " + ActualTitle);
+	}
+	
+	@BeforeMethod
+	public void Before()
+	{
+		System.out.println("Starting the Browers to Launch");
+	}
+	
+	@AfterMethod
+	public void After()
+	{
+	    driver.quit();
+		System.out.println("The Launch Completed");
+	}
+}
